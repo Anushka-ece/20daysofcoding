e7a668f2f18a58006aa539c12d3a973fb67238dc
Anushka Sharma <anushkaece007@gmail.com>
2022-01-17 19:45:35 +0530

    Create p11.cpp

---
+++ p11.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool isValid(string s) {
+         stack<char> st;  // initializing an empty stack
+        for(int i=0;i<s.length();i++){
+            if(s[i]=='(' || s[i]=='{' || s[i]=='[')
+                 st.push(s[i]); // whenever any opening bracket comes we would push the element
+            else
+            {
+                if(st.empty()) return false; // checking whether the stack has any element or not, ie. if a closing bracket comes before opening
+				
+				// conditions to check whether the given(ques) conditions are true or not
+                if(s[i]==')' && st.top()!='(')
+                    return false; 
+                if(s[i]=='}' && st.top()!='{')
+                    return false; 
+                if(s[i]==']' && st.top()!='[')
+                    return false; 
+                st.pop(); // finally popping the opening bracket
+            }
+        }
+       if(st.empty())
+           return true;
+        return false;
+    }
+};
